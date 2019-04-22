1d88e70335f0b8522d9e0f9e3c50663579a7d2dc
Yury Petrov <ypetrov@yandex-team.ru>
2019-04-22 12:53:49 +0500

    use pytest-django client fixture

--- app/currencies/tests/test_views.py
+++ app/currencies/tests/test_views.py
@@ -1,33 +1,32 @@
 import pytest
-from django.test import Client
 
 from currencies.models import ExchangeRate
 
 pytestmark = pytest.mark.django_db
 
 
-def test_get_rate_404():
-    assert Client().get("/rates/FOO/BAR/").status_code == 404
+def test_get_rate_404(client):
+    assert client.get("/rates/FOO/BAR/").status_code == 404
 
     ExchangeRate.objects.create(symbol="FOO", value=1)
-    assert Client().get("/rates/FOO/BAR/").status_code == 404
+    assert client.get("/rates/FOO/BAR/").status_code == 404
 
 
-def test_get_rate_400():
-    ExchangeRate.objects.create(symbol="FOO", value=1)
-    ExchangeRate.objects.create(symbol="BAR", value=10)
-
-    assert Client().get("/rates/FOO/BAR/?value=invalid").status_code == 400
-
-
-def test_get_rate():
+def test_get_rate_400(client):
     ExchangeRate.objects.create(symbol="FOO", value=1)
     ExchangeRate.objects.create(symbol="BAR", value=10)
 
-    response = Client().get("/rates/FOO/BAR/")
+    assert client.get("/rates/FOO/BAR/?value=invalid").status_code == 400
+
+
+def test_get_rate(client):
+    ExchangeRate.objects.create(symbol="FOO", value=1)
+    ExchangeRate.objects.create(symbol="BAR", value=10)
+
+    response = client.get("/rates/FOO/BAR/")
     assert response.status_code == 200
     assert response.data == {"source": {"value": 1, "symbol": "FOO"}, "result": {"value": 10, "symbol": "BAR"}}
 
-    response = Client().get("/rates/FOO/BAR/?value=1.5")
+    response = client.get("/rates/FOO/BAR/?value=1.5")
     assert response.status_code == 200
     assert response.data == {"source": {"value": 1.5, "symbol": "FOO"}, "result": {"value": 15, "symbol": "BAR"}}
